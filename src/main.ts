e1ce925836947e005211a6b38ccab37bb1517ad6
Ayanori Toyoda <rodrigotoyoda@outlook.com>
2023-09-21 15:31:23 +0900

    add swagger api

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,22 @@
 import { ValidationPipe } from "@nestjs/common";
 import { NestFactory } from "@nestjs/core";
+import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
 
 import { AppModule } from "./app.module";
 
 async function bootstrap() {
 	const app = await NestFactory.create(AppModule);
 	app.useGlobalPipes(new ValidationPipe());
-	await app.listen(3000);
+
+	const config = new DocumentBuilder()
+		.setTitle("Eaglys test API")
+		.setVersion("1.0")
+		.build();
+
+	const document = SwaggerModule.createDocument(app, config);
+	SwaggerModule.setup("api", app, document);
+
+	await app.listen(8000);
 }
 
 // eslint-disable-next-line @typescript-eslint/no-floating-promises
