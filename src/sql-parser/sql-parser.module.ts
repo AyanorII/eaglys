7eae6041715a8775046ed3ec942493e5ae35b34d
Ayanori Toyoda <rodrigotoyoda@outlook.com>
2023-09-25 13:22:49 +0900

    remove unused controller

--- src/sql-parser/sql-parser.module.ts
+++ src/sql-parser/sql-parser.module.ts
@@ -2,12 +2,10 @@ import { Module } from "@nestjs/common";
 import { Parser } from "node-sql-parser";
 import { HashModule } from "src/hash/hash.module";
 
-import { SqlParserController } from "./sql-parser.controller";
 import { SqlParserService } from "./sql-parser.service";
 
 @Module({
 	imports: [HashModule],
-	controllers: [SqlParserController],
 	providers: [
 		SqlParserService,
 		{
